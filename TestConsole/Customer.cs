307a99622a8c6190ee3078fbc66924276a7b9314
Vlada Davydova <davudovavlada@gmail.com>
2024-04-23 14:15:00 +0500

    Тестовый консольный проект

---
+++ TestConsole/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    internal class Customer : User
+    {
+        public Customer(int id, string login, string password, string name, string address, string city) : base (id, login, password)
+        {
+            Name = name;
+            Address = address;
+            City = city;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+
+
+    }
+}
